9c535456f24c4b06c0eb6463643e3380272aa9ff
Lovro Markovic <lovro.markovic@fer.hr>
2020-11-16 23:26:09 +0100

    [CI] Use install scripts

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -1,11 +1,14 @@
-  #!/bin/bash
+#!/bin/bash
   
-  apt-get update
-  apt-get install -y --no-install-recommends dialog apt-utils
-  apt-get -y install sudo gnupg2 apt-utils libterm-readline-gnu-perl lsb-release
-  DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
-  sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $ROS_CI_DESKTOP main\" > /etc/apt/sources.list.d/ros-latest.list"
-  sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-  sudo apt-get update
-  sudo apt-get install -y dpkg
-  sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-ros-base
+apt-get update
+apt-get install -y --no-install-recommends dialog apt-utils
+apt-get -y install sudo gnupg2 apt-utils libterm-readline-gnu-perl lsb-release
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
+sudo apt-get -y update -qq
+
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
+sudo apt-get -y upgrade --fix-missing
+sudo apt-get -y install git
--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM ubuntu:bionic
 
 # Copy environment from travis
-ENV ROS_CI_DESKTOP="`lsb_release -cs`"
-ENV CI_SOURCE_PATH=$(pwd)
 ENV ROSINSTALL_FILE=$CI_SOURCE_PATH/dependencies.rosinstall
 ENV CATKIN_OPTIONS=$CI_SOURCE_PATH/catkin.options
 ENV ROS_PARALLEL_JOBS='-j8 -l6'   
 ENV PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/dist-packages
 ENV ROS_DISTRO="melodic"
 
-COPY .ci/ci_before_install.sh travis/
-RUN ./travis/ci_before_install.sh
+COPY .ci/ci_before_install.sh setup/
+RUN ./setup/ci_before_install.sh
+
+COPY installation/ setup/
+RUN ./setup/install.sh
+RUN ./setup/workspace_setup.sh
 
 # To stall
 CMD tail -f /dev/null

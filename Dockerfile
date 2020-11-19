5ad43bc75e914fd66c7afba704f53af733f42fd5
Lovro Markovic <lovro.markovic@fer.hr>
2020-11-19 08:43:45 +0100

    Simplify installation

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,6 @@ ENV ROS_PARALLEL_JOBS='-j8 -l6'
 ENV PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/dist-packages
 ENV ROS_DISTRO="melodic"
 
-COPY .ci/ci_before_install.sh setup/
-RUN ./setup/ci_before_install.sh
-
 COPY installation/ setup/
 RUN ./setup/install.sh
 RUN ./setup/workspace_setup.sh
--- installation/install.sh
+++ installation/install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y --no-install-recommends dialog apt-utils
+apt-get -y install sudo gnupg2 apt-utils libterm-readline-gnu-perl lsb-release
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
+sudo apt-get -y update -qq
+
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
+sudo apt-get -y upgrade --fix-missing
+sudo apt-get -y install git
+
 # get the path to this script
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`

9b7c332337ae9900e03601d0136306f41fc66c63
Lovro Markovic <lovro.markovic@fer.hr>
2021-02-16 12:55:20 +0100

    Remove unused lines from install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -9,17 +9,9 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # Executes a command when ERR signal is emmitted in this script
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends tzdata
-
-# the "gce-compute-image-packages" package often freezes the installation at some point
-# the installation freezes when it tries to manage some systemd services
-# this attempts to install the package and stop the problematic service during the process
-((sleep 90 && (sudo systemctl stop google-instance-setup.service && echo "gce service stoped" || echo "gce service not stoped")) & (sudo timeout 120s apt-get -y install gce-compute-image-packages)) || echo "\e[1;31mInstallation of gce-compute-image-packages failed\e[0m"
-
 distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
-[ "$distro" = "20.04" ] && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends keyboard-configuration
 
 # get the path to this script
 MY_PATH=`dirname "$0"`
